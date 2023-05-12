050f8eeeb33834c610cf9b6405c640740ed6372c
Vladik <vladkramarenko53@gmail.com>
2023-05-12 15:37:48 +0300

    update 1.1

--- PythonApplication10/PythonApplication10.py
+++ PythonApplication10/PythonApplication10.py
@@ -1,4 +1,6 @@
-﻿def read_int_input(message):
+﻿from datetime import datetime
+from dateutil.relativedelta import relativedelta
+def read_int_input(message):
     while True:
         try:
             value = input(message)
@@ -36,30 +38,43 @@ def read_deposit_term():
 
 def calculate_monthly_interest(deposit_amount, deposit_term):
     annual_interest_rate = 20.0  # 20% річних
-    interest_rate = float(annual_interest_rate) / 12.0 / 100.0  # обчислення місячної ставки відсотка
-    total_amount = deposit_amount  # загальна сума на вкладі
+    interest_rate = annual_interest_rate / 12.0 / 100.0  # обчислення місячної ставки відсотка
     interest_schedule = []  # графік нарахувань
     total_interest = 0  # перемінна для сумування значення interest
     amount = deposit_amount  # змінна для розрахунку підсумкової суми на рахунку вкладу
+    start_date = datetime.now()
+    total_amount = deposit_amount  # загальна сума на вкладі
 
-    for month in range(1, deposit_term  + 1):
+    for month in range(1, deposit_term + 1):
         interest = total_amount * interest_rate
         total_interest += interest
         amount += interest  # збільшення загальної суми на вкладі
         if month % 12 == 0:
             total_amount += total_interest
             total_interest = 0
-        interest_schedule.append((month, round(interest, 2), round(amount, 2), round(total_amount, 2)))
+        current_date_parts = start_date.strftime("%d/%m/%Y").split("/")
+        month_int = int(current_date_parts[1])
+        year_int = int(current_date_parts[2])
+        month_int += 1
+        if month_int > 12:
+            month_int = 1
+            year_int += 1
+        start_date = start_date + relativedelta(months=1)
+        current_date_str = start_date.strftime("%d/%m/%Y")
+        interest_schedule.append((month, current_date_str, round(interest, 2), round(amount, 2)))
 
     return interest_schedule, round(total_amount, 2)
+
+
+
 def print_interest_schedule(interest_schedule):
     """
     Функция для вывода графика начислений на экран в виде таблицы.
     """
-    print("|{: <10}|{: <15}|{: <20}|{: <30}|".format("Місяць", "Сума нарахувань", "Поточна сума", "Загальна сума на вкладі"))
-    print("|{: <10}|{: <15}|{: <20}|{: <30}|".format("-"*10, "-"*15, "-"*20, "-"*30))
-    for month, interest, amount, total_amount in interest_schedule:
-        print("|{: <10}|{: <15}|{: <20}|{: <30}|".format(month, interest, amount, total_amount))
+    print("|{: ^10}|{: ^10}|{: >15}|{: >20}|".format("Місяць", "Дата", "Сума нарахувань", "Поточна сума"))
+    print("|{: ^10}|{: ^10}|{: >15}|{: >20}|".format("-"*10, "-"*10, "-"*15, "-"*20, ))
+    for month, current_date, interest, amount in interest_schedule:
+        print("|{: ^10}|{: ^10}|{: >15}|{: >20}|".format(month, current_date, interest, amount))
 
 print("Калькулятор нарахування відсотків на вкладі")
 print("---------------------------------------------")
@@ -71,4 +86,5 @@ interest_schedule, total_amount = calculate_monthly_interest(deposit_amount, dep
 profit = total_amount - deposit_amount
 
 print_interest_schedule(interest_schedule)
-print("Клієнт заробив:", profit)
+print("Банк віддав ", total_amount)
+print("Клієнт заробив" ,profit)
